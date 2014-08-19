0cfe9cc749bcb4d1d1e7333611c06a0ceba8e5eb
Saygın Burak <sayginburak17@gmail.com>
2014-08-19 19:35:16 +0300

    New user tamamlandı

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,4 +8,18 @@ class UsersController < ApplicationController
   def show
     @user=User.find(params[:id])
   end
+
+  def create
+    @user=User.new(strong_params)
+    if @user.save
+      redirect_to(@user)
+    else
+      render "new"
+    end
+
+  end
+  private
+  def strong_params
+    params.require(:user).permit(:name,:surname,:email,:bio)
+  end
 end
