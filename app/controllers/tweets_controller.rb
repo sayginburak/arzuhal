31adb0a49a1d4f6ec30b46cd13340e4910370561
Saygın Burak <sayginburak17@gmail.com>
2014-08-22 17:19:13 +0300

    Navbar eklendi ve daha bir suru şey

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,14 +1,17 @@
 class TweetsController < ApplicationController
+  http_basic_authenticate_with name: "sbs", password: "123456", except: [:index, :show]
+
   def index
-    @tweets=Tweet.all
+    @tweets=Tweet.paginate(:page => params[:page], :per_page => 5)
+
   end
   def show
     @tweet=Tweet.find(params[:id])
   end
   def create
     @user=User.find(params[:user_id])
-    @tweet=@user.tweets.create(tweet_params)
-    redirect_to(user_path(@user))
+    @tweet=@user.tweets.new(tweet_params)
+    respond_with @user
   end
   def destroy
     @tweet=Tweet.find(params[:id])
@@ -20,6 +23,4 @@ class TweetsController < ApplicationController
   def tweet_params
     params.require(:tweet).permit(:text)
   end
-
-
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,34 +1,30 @@
 class UsersController < ApplicationController
+  http_basic_authenticate_with name: "sbs", password: "123456", except: [:index, :show]
   def index
     @users=User.all
   end
+
   def new
     @user=User.new
   end
+
   def show
     @user=User.find(params[:id])
+    @tweet=@user.tweets.new
   end
 
   def create
-    @user=User.new(strong_params)
-    if @user.save
-      redirect_to(@user)
-    else
-      render "new"
-    end
-
+    @user=User.create(strong_params)
+    respond_with @user
   end
 
   def edit
     @user=User.find(params[:id])
   end
+
   def update
     @user=User.find(params[:id])
-    if @user.update(strong_params)
-      redirect_to(@user)
-    else
-      render "edit"
-    end
+    respond_with @user
   end
 
   def destroy
@@ -36,6 +32,7 @@ class UsersController < ApplicationController
     @user.destroy
     redirect_to(@user)
   end
+
   private
   def strong_params
     params.require(:user).permit(:name,:surname,:email,:bio)
